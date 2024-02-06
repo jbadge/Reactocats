3c1c692ee7b0387a599595835af27fbf67010e07
Jeremy Badger <jeremebadger@gmail.com>
2024-02-06 09:40:48 -0800

    Imported html from Octodex

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,467 @@
 import React from 'react'
 
 export function App() {
-  return <div>Hello, World </div>
+  return (
+    <main>
+      <article>
+        <div className="wrapper">
+          <div className="entry">
+            <a href="https://octodex.github.com/parentocats/">
+              <img
+                src="https://octodex.github.com/images/parentocats.png"
+                alt="Parentocats"
+              />
+            </a>
+            <ul>
+              <li>
+                #155:{' '}
+                <a href="https://octodex.github.com/parentocats/">
+                  <strong>Parentocats</strong>
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/johncreek">
+                  <img
+                    src="https://github.com/johncreek.png"
+                    width="24px"
+                    height="24px"
+                    alt="johncreek"
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="entry">
+            <a href="https://octodex.github.com/godotocat/">
+              <img
+                src="https://octodex.github.com/images/godotocat.png"
+                alt="Godotocat"
+              />
+            </a>
+            <ul>
+              <li>
+                #154:{' '}
+                <a href="https://octodex.github.com/godotocat/">
+                  <strong>Godotocat</strong>
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/johncreek">
+                  <img
+                    src="https://github.com/johncreek.png"
+                    width="24px"
+                    height="24px"
+                    alt="johncreek"
+                  />
+                </a>
+                <a href="https://github.com/leereilly">
+                  <img
+                    src="https://github.com/leereilly.png"
+                    width="24px"
+                    height="24px"
+                    alt="leereilly"
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="entry">
+            <a href="https://octodex.github.com/nuxtocat/">
+              <img
+                src="https://octodex.github.com/images/NUX_Octodex.gif"
+                alt="NUXtocat"
+              />
+            </a>
+            <ul>
+              <li>
+                #153:{' '}
+                <a href="https://octodex.github.com/nuxtocat/">
+                  <strong>NUXtocat</strong>
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/cameronfoxly">
+                  <img
+                    src="https://github.com/cameronfoxly.png"
+                    width="24px"
+                    height="24px"
+                    alt="cameronfoxly"
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="entry">
+            <a href="https://octodex.github.com/yogitocat/">
+              <img
+                src="https://octodex.github.com/images/yogitocat.png"
+                alt="Yogitocat"
+              />
+            </a>
+            <ul>
+              <li>
+                #152:{' '}
+                <a href="https://octodex.github.com/yogitocat/">
+                  <strong>Yogitocat</strong>
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/johncreek">
+                  <img
+                    src="https://github.com/johncreek.png"
+                    width="24px"
+                    height="24px"
+                    alt="johncreek"
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="entry">
+            <a href="https://octodex.github.com/mona-the-rivetertocat/">
+              <img
+                src="https://octodex.github.com/images/mona-the-rivetertocat.png"
+                alt="Mona the Rivertertocat"
+              />
+            </a>
+            <ul>
+              <li>
+                #151:{' '}
+                <a href="https://octodex.github.com/mona-the-rivetertocat/">
+                  <strong>Mona the Rivetertocat</strong>
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/johncreek">
+                  <img
+                    src="https://github.com/johncreek.png"
+                    width="24px"
+                    height="24px"
+                    alt="johncreek"
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="entry">
+            <a href="https://octodex.github.com/manufacturetocat/">
+              <img
+                src="https://octodex.github.com/images/manufacturetocat.png"
+                alt="Manufacturetocat"
+              />
+            </a>
+            <ul>
+              <li>
+                #150:{' '}
+                <a href="https://octodex.github.com/manufacturetocat/">
+                  <strong>Manufacturetocat</strong>
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/heyhayhay">
+                  <img
+                    src="https://github.com/heyhayhay.png"
+                    width="24px"
+                    height="24px"
+                    alt="heyhayhay"
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="entry">
+            <a href="https://octodex.github.com/octoasians/">
+              <img
+                src="https://octodex.github.com/images/OctoAsians_dex_Full.png"
+                alt="OctoAsians"
+              />
+            </a>
+            <ul>
+              <li>
+                #149:{' '}
+                <a href="https://octodex.github.com/octoasians/">
+                  <strong>OctoAsians</strong>
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/cameronfoxly">
+                  <img
+                    src="https://github.com/cameronfoxly.png"
+                    width="24px"
+                    height="24px"
+                    alt="cameronfoxly"
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="entry">
+            <a href="https://octodex.github.com/octoqueer/">
+              <img
+                src="https://octodex.github.com/images/Octoqueer.png"
+                alt="Octoqueer"
+              />
+            </a>
+            <ul>
+              <li>
+                #148:{' '}
+                <a href="https://octodex.github.com/octoqueer/">
+                  <strong>Octoqueer</strong>
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/cameronfoxly">
+                  <img
+                    src="https://github.com/cameronfoxly.png"
+                    width="24px"
+                    height="24px"
+                    alt="cameronfoxly"
+                  />
+                </a>
+                <a href="https://github.com/johncreek">
+                  <img
+                    src="https://github.com/johncreek.png"
+                    width="24px"
+                    height="24px"
+                    alt="johncreek"
+                  />
+                </a>
+                <a href="https://github.com/tonyjaramillo">
+                  <img
+                    src="https://github.com/tonyjaramillo.png"
+                    width="24px"
+                    height="24px"
+                    alt="tonyjaramillo"
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="entry">
+            <a href="https://octodex.github.com/terracottocat/">
+              <img
+                src="https://octodex.github.com/images/Terracottocat_Single.png"
+                alt="Terracottocat"
+              />
+            </a>
+            <ul>
+              <li>
+                #147:{' '}
+                <a href="https://octodex.github.com/terracottocat/">
+                  <strong>Terracottocat</strong>
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/chubbmo">
+                  <img
+                    src="https://github.com/chubbmo.png"
+                    width="24px"
+                    height="24px"
+                    alt="chubbmo"
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="entry">
+            <a href="https://octodex.github.com/octogatos/">
+              <img
+                src="https://octodex.github.com/images/Octogatos.png"
+                alt="Octogatos"
+              />
+            </a>
+            <ul>
+              <li>
+                #146:{' '}
+                <a href="https://octodex.github.com/octogatos/">
+                  <strong>Octogatos</strong>
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/cameronfoxly">
+                  <img
+                    src="https://github.com/cameronfoxly.png"
+                    width="24px"
+                    height="24px"
+                    alt="cameronfoxly"
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="entry">
+            <a href="https://octodex.github.com/adacats/">
+              <img
+                src="https://octodex.github.com/images/Adacats.png"
+                alt="Adacats"
+              />
+            </a>
+            <ul>
+              <li>
+                #145:{' '}
+                <a href="https://octodex.github.com/adacats/">
+                  <strong>Adacats</strong>
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/cameronfoxly">
+                  <img
+                    src="https://github.com/cameronfoxly.png"
+                    width="24px"
+                    height="24px"
+                    alt="cameronfoxly"
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="entry">
+            <a href="https://octodex.github.com/fintechtocat/">
+              <img
+                src="https://octodex.github.com/images/Fintechtocat.png"
+                alt="Fintechtocat"
+              />
+            </a>
+            <ul>
+              <li>
+                #144:{' '}
+                <a href="https://octodex.github.com/fintechtocat/">
+                  <strong>Fintechtocat</strong>
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/ceciliorz">
+                  <img
+                    src="https://github.com/ceciliorz.png"
+                    width="24px"
+                    height="24px"
+                    alt="ceciliorz"
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="entry">
+            <a href="https://octodex.github.com/puppeteer/">
+              <img
+                src="https://octodex.github.com/images/puppeteer.png"
+                alt="Puppeteer"
+              />
+            </a>
+            <ul>
+              <li>
+                #4:{' '}
+                <a href="https://octodex.github.com/puppeteer/">
+                  <strong>Puppeteer</strong>
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/cameronmcefee">
+                  <img
+                    src="https://github.com/cameronmcefee.png"
+                    width="24px"
+                    height="24px"
+                    alt="cameronmcefee"
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="entry">
+            <a href="https://octodex.github.com/octobiwan/">
+              <img
+                src="https://octodex.github.com/images/octobiwan.jpg"
+                alt="Octobi Wan Catnobi"
+              />
+            </a>
+            <ul>
+              <li>
+                #3:{' '}
+                <a href="https://octodex.github.com/octobiwan/">
+                  <strong>Octobi Wan Catnobi</strong>
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/cameronmcefee">
+                  <img
+                    src="https://github.com/cameronmcefee.png"
+                    width="24px"
+                    height="24px"
+                    alt="cameronmcefee"
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="entry">
+            <a href="https://octodex.github.com/className-act/">
+              <img
+                src="https://octodex.github.com/images/className-act.png"
+                alt="Class Act"
+              />
+            </a>
+            <ul>
+              <li>
+                #2:{' '}
+                <a href="https://octodex.github.com/className-act/">
+                  <strong>Class Act</strong>
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/cameronmcefee">
+                  <img
+                    src="https://github.com/cameronmcefee.png"
+                    width="24px"
+                    height="24px"
+                    alt="cameronmcefee"
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="entry">
+            <a href="https://octodex.github.com/original/">
+              <img
+                src="https://octodex.github.com/images/original.png"
+                alt="Original"
+              />
+            </a>
+            <ul>
+              <li>
+                #1:{' '}
+                <a href="https://octodex.github.com/original/">
+                  <strong>Original</strong>
+                </a>
+              </li>
+              <li>
+                <a href="http://www.idokungfoo.com/">
+                  <img
+                    src="https://octodex.github.com/images/idokungfoo-avatar.jpg"
+                    width="24px"
+                    height="24px"
+                    alt="idokungfoo"
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </article>
+    </main>
+  )
 }
